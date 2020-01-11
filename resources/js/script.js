efe592e497ba4dd366ab0e8fc4a50f583603e11b
Saria <jaanamatil@gmail.com>
2020-01-11 23:55:50 +0200

    Faroe init - needs refactoring

---
+++ resources/js/script.js
@@ -0,0 +1,12 @@
+$(".vertical-center").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true
+  });
+
+  $(".wildlife-gallery").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true
+  });
